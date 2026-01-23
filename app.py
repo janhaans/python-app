52a8264b9524a23da0ded2e209739a92ce709f7b
Jan Haans <jan.haans@gmail.com>
2026-01-23 16:17:03 +0100

    Strip to bare application code

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/api/v1/details', methods=['GET'])
 def details():
     return {
-        'hostname': socket.gethostname(),
+        'message': 'Hello, World!',
         'time': datetime.now().isoformat()
     }
 
@@ -16,4 +16,4 @@ def healthz():
     return {'status': 'up'}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
